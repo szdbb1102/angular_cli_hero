491abc22c3379f163b1b4f068f641d520be092f5
xx23855 <xx23855@ly.com>
2017-12-26 11:57:52 +0800

    使用service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -14,17 +16,25 @@ import { HEROES } from '../mock-heroes';
   })
   export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
-
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
+
+// 使用service
+// 1. import
+// 2. 放到constructor
+// 3. 引用形式this.heroService.getHeroes();
