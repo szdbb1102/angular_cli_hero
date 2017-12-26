b5b953951fc30a3b6829d371581b3b555891cdd0
xx23855 <xx23855@ly.com>
2017-12-26 10:50:55 +0800

    创建和引用类

---
+++ src/app/hero.ts
@@ -0,0 +1,7 @@
+// 命令行 ：ng g class Hero
+
+export class Hero {
+    id: number;
+    name: string;
+}
+
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,10 @@
+// 创建hero类
+// 引入Hero类
+// 属性中继承该类
+// html绑定数据格式改变
+
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +13,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 
