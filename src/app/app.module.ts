5a92a7222b38f17b19fc28d79d34d3e1cf7e4efb
xx23855 <xx23855@ly.com>
2017-12-26 11:43:55 +0800

    子组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// 引入FormsModule，在@NgModul 中 imports加入FormsModule，则可以双向绑定
 import { FormsModule } from '@angular/forms';
 
-// Every component must be declared in exactly one NgModule.
-// 命令行生成组件时这个工作已经帮我们做好了，具体就是在app.module引入，在declarations中加入
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     FormsModule,
