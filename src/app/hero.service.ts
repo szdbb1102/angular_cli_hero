991ab879fbfb6ab06fef0e62178e85a65d90f4d8
xx23855 <xx23855@ly.com>
2017-12-26 15:15:30 +0800

    heroService模拟接口访问

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,19 +6,26 @@ import { MessageService } from './message.service';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
-  constructor(private messageService: MessageService) { }
-
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
   getHero(id: number): Observable<Hero> {
     // Todo: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 
 }
